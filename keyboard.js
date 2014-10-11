0e1a484c4f65f0d5207ee65df70018dd88402830
Gilbert Virgo <gilbertjvirgo1@gmail.com>
2014-10-11 09:53:05 +0100

    A JavaScript keyboard plugin
    Keyboard.js is a keyboard plugin in development stage. The aim of the
    plugin is to create an easy to use interface for working with the
    keyboard in JavaScript.

---
+++ keyboard.js
@@ -0,0 +1,389 @@
+/*
+ * keyboard.js
+ *
+ * Created by Gilbert Virgo
+*/
+
+var Keyboard = {
+    // Keys 
+    A: {
+        getKeyCode: function() {
+            return 65
+        },
+        down: function(func) {
+            document.addEventListener('keydown', func, false);
+        },
+        up: function(func) {
+            document.addEventListener('keyup', func, false);
+        },
+        press: function(func) {
+            document.addEventListener('keypress', func, false);
+        }
+    },
+    B: {
+        getKeyCode: function() {
+            return 66
+        },
+        down: function(func) {
+            document.addEventListener('keydown', func, false);
+        },
+        up: function(func) {
+            document.addEventListener('keyup', func, false);
+        },
+        press: function(func) {
+            document.addEventListener('keypress', func, false);
+        }
+    },
+    C: {
+        getKeyCode: function() {
+            return 67
+        },
+        down: function(func) {
+            document.addEventListener('keydown', func, false);
+        },
+        up: function(func) {
+            document.addEventListener('keyup', func, false);
+        },
+        press: function(func) {
+            document.addEventListener('keypress', func, false);
+        }
+    },
+    D: {
+        getKeyCode: function() {
+            return 68
+        },
+        down: function(func) {
+            document.addEventListener('keydown', func, false);
+        },
+        up: function(func) {
+            document.addEventListener('keyup', func, false);
+        },
+        press: function(func) {
+            document.addEventListener('keypress', func, false);
+        }
+    },
+    E: {
+        getKeyCode: function() {
+            return 69
+        },
+        down: function(func) {
+            document.addEventListener('keydown', func, false);
+        },
+        up: function(func) {
+            document.addEventListener('keyup', func, false);
+        },
+        press: function(func) {
+            document.addEventListener('keypress', func, false);
+        }
+    },
+    F: {
+        getKeyCode: function() {
+            return 70
+        },
+        down: function(func) {
+            document.addEventListener('keydown', func, false);
+        },
+        up: function(func) {
+            document.addEventListener('keyup', func, false);
+        },
+        press: function(func) {
+            document.addEventListener('keypress', func, false);
+        }
+    },
+    G: {
+        getKeyCode: function() {
+            return 71
+        },
+        down: function(func) {
+            document.addEventListener('keydown', func, false);
+        },
+        up: function(func) {
+            document.addEventListener('keyup', func, false);
+        },
+        press: function(func) {
+            document.addEventListener('keypress', func, false);
+        }
+    },
+    H: {
+        getKeyCode: function() {
+            return 72
+        },
+        down: function(func) {
+            document.addEventListener('keydown', func, false);
+        },
+        up: function(func) {
+            document.addEventListener('keyup', func, false);
+        },
+        press: function(func) {
+            document.addEventListener('keypress', func, false);
+        }
+    },
+    I: {
+        getKeyCode: function() {
+            return 73
+        },
+        down: function(func) {
+            document.addEventListener('keydown', func, false);
+        },
+        up: function(func) {
+            document.addEventListener('keyup', func, false);
+        },
+        press: function(func) {
+            document.addEventListener('keypress', func, false);
+        }
+    },
+    J: {
+        getKeyCode: function() {
+            return 74
+        },
+        down: function(func) {
+            document.addEventListener('keydown', func, false);
+        },
+        up: function(func) {
+            document.addEventListener('keyup', func, false);
+        },
+        press: function(func) {
+            document.addEventListener('keypress', func, false);
+        }
+    },
+    K: {
+        getKeyCode: function() {
+            return 75
+        },
+        down: function(func) {
+            document.addEventListener('keydown', func, false);
+        },
+        up: function(func) {
+            document.addEventListener('keyup', func, false);
+        },
+        press: function(func) {
+            document.addEventListener('keypress', func, false);
+        }
+    },
+    L: {
+        getKeyCode: function() {
+            return 76
+        },
+        down: function(func) {
+            document.addEventListener('keydown', func, false);
+        },
+        up: function(func) {
+            document.addEventListener('keyup', func, false);
+        },
+        press: function(func) {
+            document.addEventListener('keypress', func, false);
+        }
+    },
+    M: {
+        getKeyCode: function() {
+            return 77
+        },
+        down: function(func) {
+            document.addEventListener('keydown', func, false);
+        },
+        up: function(func) {
+            document.addEventListener('keyup', func, false);
+        },
+        press: function(func) {
+            document.addEventListener('keypress', func, false);
+        }
+    },
+    N: {
+        getKeyCode: function() {
+            return 78
+        },
+        down: function(func) {
+            document.addEventListener('keydown', func, false);
+        },
+        up: function(func) {
+            document.addEventListener('keyup', func, false);
+        },
+        press: function(func) {
+            document.addEventListener('keypress', func, false);
+        }
+    },
+    O: {
+        getKeyCode: function() {
+            return 79
+        },
+        down: function(func) {
+            document.addEventListener('keydown', func, false);
+        },
+        up: function(func) {
+            document.addEventListener('keyup', func, false);
+        },
+        press: function(func) {
+            document.addEventListener('keypress', func, false);
+        }
+    },
+    P: {
+        getKeyCode: function() {
+            return 80
+        },
+        down: function(func) {
+            document.addEventListener('keydown', func, false);
+        },
+        up: function(func) {
+            document.addEventListener('keyup', func, false);
+        },
+        press: function(func) {
+            document.addEventListener('keypress', func, false);
+        }
+    },
+    Q: {
+        getKeyCode: function() {
+            return 81
+        },
+        down: function(func) {
+            document.addEventListener('keydown', func, false);
+        },
+        up: function(func) {
+            document.addEventListener('keyup', func, false);
+        },
+        press: function(func) {
+            document.addEventListener('keypress', func, false);
+        }
+    },
+    R: {
+        getKeyCode: function() {
+            return 82
+        },
+        down: function(func) {
+            document.addEventListener('keydown', func, false);
+        },
+        up: function(func) {
+            document.addEventListener('keyup', func, false);
+        },
+        press: function(func) {
+            document.addEventListener('keypress', func, false);
+        }
+    },
+    S: {
+        getKeyCode: function() {
+            return 83
+        },
+        down: function(func) {
+            document.addEventListener('keydown', func, false);
+        },
+        up: function(func) {
+            document.addEventListener('keyup', func, false);
+        },
+        press: function(func) {
+            document.addEventListener('keypress', func, false);
+        }
+    },
+    T: {
+        getKeyCode: function() {
+            return 84
+        },
+        down: function(func) {
+            document.addEventListener('keydown', func, false);
+        },
+        up: function(func) {
+            document.addEventListener('keyup', func, false);
+        },
+        press: function(func) {
+            document.addEventListener('keypress', func, false);
+        }
+    },
+    U: {
+        getKeyCode: function() {
+            return 85
+        },
+        down: function(func) {
+            document.addEventListener('keydown', func, false);
+        },
+        up: function(func) {
+            document.addEventListener('keyup', func, false);
+        },
+        press: function(func) {
+            document.addEventListener('keypress', func, false);
+        }
+    },
+    V: {
+        getKeyCode: function() {
+            return 86
+        },
+        down: function(func) {
+            document.addEventListener('keydown', func, false);
+        },
+        up: function(func) {
+            document.addEventListener('keyup', func, false);
+        },
+        press: function(func) {
+            document.addEventListener('keypress', func, false);
+        }
+    },
+    W: {
+        getKeyCode: function() {
+            return 87
+        },
+        down: function(func) {
+            document.addEventListener('keydown', func, false);
+        },
+        up: function(func) {
+            document.addEventListener('keyup', func, false);
+        },
+        press: function(func) {
+            document.addEventListener('keypress', func, false);
+        }
+    },
+    X: {
+        getKeyCode: function() {
+            return 88
+        },
+        down: function(func) {
+            document.addEventListener('keydown', func, false);
+        },
+        up: function(func) {
+            document.addEventListener('keyup', func, false);
+        },
+        press: function(func) {
+            document.addEventListener('keypress', func, false);
+        }
+    },
+    Y: {
+        getKeyCode: function() {
+            return 89
+        },
+        down: function(func) {
+            document.addEventListener('keydown', func, false);
+        },
+        up: function(func) {
+            document.addEventListener('keyup', func, false);
+        },
+        press: function(func) {
+            document.addEventListener('keypress', func, false);
+        }
+    },
+    Z: {
+        getKeyCode: function() {
+            return 90
+        },
+        down: function(func) {
+            document.addEventListener('keydown', func, false);
+        },
+        up: function(func) {
+            document.addEventListener('keyup', func, false);
+        },
+        press: function(func) {
+            document.addEventListener('keypress', func, false);
+        }
+    },
+    // General listeners
+    down: function(func) {
+        document.addEventListener("keydown", func, false);
+    },
+    up: function(func) {
+        document.addEventListener("keyup", func, false);
+    },
+    press: function(func) {
+        document.addEventListener("keypress", func, false);
+    },
+    getKeyCode: function(e) {
+        return e.which;
+    },
+    getKeyChar: function(e) {
+        return (e.which) ? String.fromCharCode(e.which) : String.fromCharCode(e.keyCode); 
+    }
+}
